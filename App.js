dd84e3d2cfe114af583163f3cb5e7ff94922ddec
Kalpesh_Desale <kalpesh.desale@demandhelm.com>
2024-11-06 01:01:12 +0530

    setup parcel

--- App.js
+++ App.js
@@ -1,15 +1,5 @@
-{
-  /* <div id='parent'>
-  <div id=' child1'>
-    <h1>Heading</h1>
-    <h2>Sub Heading</h2>
-  </div>
-  <div id=' child2'>
-    <h1>Heading</h1>
-    <h2>Sub Heading</h2>
-  </div>
-</div> */
-}
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 const heading = React.createElement('h1', {}, 'Heading');
 const subHeading = React.createElement('h2', {}, 'Sub Heading');
 const child1 = React.createElement('div', { id: 'child1' }, [
