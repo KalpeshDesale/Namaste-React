30e5c7afff24cd9d84a205e0eb76118f5c3eb1d6
Kalpesh_Desale <kalpesh.desale@demandhelm.com>
2024-11-15 21:19:27 +0530

    basic food delivery UI

--- App.js
+++ App.js
@@ -1,16 +1,115 @@
 import React from 'react';
+import { Star } from 'lucide-react';
+
 import ReactDOM from 'react-dom/client';
-const heading = React.createElement('h1', {}, 'Heading');
-const subHeading = React.createElement('h2', {}, 'Sub Heading');
-const child1 = React.createElement('div', { id: 'child1' }, [
-  heading,
-  subHeading,
-]);
-const child2 = React.createElement('div', { id: 'child2' }, [
-  heading,
-  subHeading,
-]);
-const parent = React.createElement('div', { id: 'parent' }, [child1, child2]);
+import './index.css';
+import { restaurantsList } from './dummyData';
+const NavBar = () => {
+  return (
+    <div className='nav-container'>
+      <ul>
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Contact Us</li>
+        <li>Cart</li>
+      </ul>
+    </div>
+  );
+};
+const Header = (
+  <div className='header-container'>
+    <div className='img-container'>
+      <img
+        src='https://marketplace.canva.com/EAF1XAgJrCg/1/0/1600w/canva-white-brown-simple-restaurant-logo-koIA1HEug0Q.jpg'
+        alt=''
+      />
+    </div>
+    <NavBar />
+  </div>
+);
+
+const SearchBar = () => (
+  <div className='search-container'>
+    <input type='text' placeholder='Search restaurant' />
+  </div>
+);
+
+const RestaurantCard = (props) => {
+  const { restaurantInfo } = props;
+  const {
+    cloudinaryImageId,
+    name,
+    cuisines,
+    avgRatingString,
+    costForTwo,
+    sla,
+  } = restaurantInfo.info;
+  console.log(restaurantInfo);
+  console.log(cloudinaryImageId);
+  return (
+    <div className='card-container'>
+      <div className='card-img'>
+        <img
+          src={
+            'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/' +
+            cloudinaryImageId
+          }
+          alt=''
+        />
+      </div>
+      <div className='content'>
+        <div className='restaurant'>{name}</div>
+        <div style={{ display: 'flex', marginBottom: '3px' }}>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <Star color='#f0c800' fill='#f0c800' size={16} />
+            <div className='rating'>{avgRatingString}</div>
+          </div>
+          <div className='eta'>{sla.slaString}</div>
+        </div>
+        <div className='cuisines'>{cuisines.join(', ')}</div>
+        <div>{costForTwo}</div>
+      </div>
+    </div>
+  );
+};
+const PageContainer = () => {
+  return (
+    <div className='page-container'>
+      <SearchBar />
+      <div className='list'>
+        {restaurantsList.map((restaurant) => (
+          <RestaurantCard restaurantInfo={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div>
+      {Header}
+      <PageContainer />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent);
+
+root.render(<AppLayout />);
+
+// const heading = React.createElement('h1', {}, 'Heading');
+// const subHeading = React.createElement('h2', {}, 'Sub Heading');
+// const child1 = React.createElement('div', { id: 'child1' }, [
+//   heading,
+//   subHeading,
+// ]);
+// const child2 = React.createElement('div', { id: 'child2' }, [
+//   heading,
+//   subHeading,
+// ]);
+// const parent = React.createElement('div', { id: 'parent' }, [child1, child2]);
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// root.render(parent);
---
+++ dummyData.js
@@ -0,0 +1,555 @@
+export const restaurantsList = [
+  {
+    info: {
+      id: '334229',
+      name: 'Pizza Hut',
+      cloudinaryImageId:
+        'RX_THUMBNAIL/IMAGES/VENDOR/2024/7/17/696f2647-8170-4b28-afd6-5f64976cc395_334229.jpg',
+      locality: 'Atul Nagar',
+      areaName: 'Warje',
+      costForTwo: '₹350 for two',
+      cuisines: ['Pizzas'],
+      avgRating: 4.2,
+      parentId: '721',
+      avgRatingString: '4.2',
+      totalRatingsString: '3.3K+',
+      sla: {
+        deliveryTime: 27,
+        lastMileTravel: 0.5,
+        serviceability: 'SERVICEABLE',
+        slaString: '25-30 mins',
+        lastMileTravelString: '0.5 km',
+        iconType: 'ICON_TYPE_EMPTY',
+      },
+      availability: {
+        nextCloseTime: '2024-11-16 03:00:00',
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: 'F',
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: '50% OFF',
+        subHeader: 'UPTO ₹100',
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: 'ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT',
+        differentiatedUiMediaDetails: {
+          mediaType: 'ADS_MEDIA_ENUM_IMAGE',
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: 'RESTAURANT_DISPLAY_TYPE_DEFAULT',
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: '--',
+        },
+      },
+      ratingsDisplayPreference: 'RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY',
+    },
+  },
+  {
+    info: {
+      id: '613996',
+      name: 'Chinese Wok',
+      cloudinaryImageId: 'e0839ff574213e6f35b3899ebf1fc597',
+      locality: 'Paud Road',
+      areaName: 'Kothrud',
+      costForTwo: '₹250 for two',
+      cuisines: ['Chinese', 'Pan-Asian', 'Tibetan'],
+      avgRating: 4.3,
+      parentId: '61955',
+      avgRatingString: '4.3',
+      totalRatingsString: '2.6K+',
+      sla: {
+        deliveryTime: 34,
+        lastMileTravel: 3,
+        serviceability: 'SERVICEABLE',
+        slaString: '30-35 mins',
+        lastMileTravelString: '3.0 km',
+        iconType: 'ICON_TYPE_EMPTY',
+      },
+      availability: {
+        nextCloseTime: '2024-11-15 23:00:00',
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: 'F',
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: '50% OFF',
+        subHeader: 'UPTO ₹100',
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: 'ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT',
+        differentiatedUiMediaDetails: {
+          mediaType: 'ADS_MEDIA_ENUM_IMAGE',
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: 'RESTAURANT_DISPLAY_TYPE_DEFAULT',
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: '--',
+        },
+      },
+      ratingsDisplayPreference: 'RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY',
+    },
+    analytics: {
+      context: 'seo-data-e28ce632-18d9-448a-9088-f4fe4b0f64ce',
+    },
+    cta: {
+      link: 'https://www.swiggy.com/city/pune/chinese-wok-paud-road-kothrud-rest613996',
+      text: 'RESTAURANT_MENU',
+      type: 'WEBLINK',
+    },
+    widgetId: 'collectionV5RestaurantListWidget_SimRestoRelevance_food_seo',
+  },
+  {
+    info: {
+      id: '765051',
+      name: 'KFC',
+      cloudinaryImageId:
+        'RX_THUMBNAIL/IMAGES/VENDOR/2024/11/5/9a8984a4-18d2-4a07-a1e1-63947e413239_765051.JPG',
+      locality: 'Mumbai Bangalore by pass',
+      areaName: 'Warje',
+      costForTwo: '₹400 for two',
+      cuisines: ['Burgers', 'Biryani', 'American', 'Snacks', 'Fast Food'],
+      avgRating: 4.2,
+      parentId: '547',
+      avgRatingString: '4.2',
+      totalRatingsString: '1.1K+',
+      sla: {
+        deliveryTime: 18,
+        lastMileTravel: 0.6,
+        serviceability: 'SERVICEABLE',
+        slaString: '15-20 mins',
+        lastMileTravelString: '0.6 km',
+        iconType: 'ICON_TYPE_EMPTY',
+      },
+      availability: {
+        nextCloseTime: '2024-11-15 23:00:00',
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: 'F',
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: '30% OFF',
+        subHeader: 'UPTO ₹75',
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: 'ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT',
+        differentiatedUiMediaDetails: {
+          mediaType: 'ADS_MEDIA_ENUM_IMAGE',
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: 'RESTAURANT_DISPLAY_TYPE_DEFAULT',
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: '--',
+        },
+      },
+      ratingsDisplayPreference: 'RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY',
+    },
+  },
+  {
+    info: {
+      id: '34929',
+      name: "Domino's Pizza",
+      cloudinaryImageId:
+        'RX_THUMBNAIL/IMAGES/VENDOR/2024/10/24/7377d3c3-8a8d-456d-94ee-1ba492c753e8_34929.JPG',
+      locality: 'Pune Mumbai Bypass Highway',
+      areaName: 'Warje',
+      costForTwo: '₹400 for two',
+      cuisines: ['Pizzas', 'Italian', 'Pastas', 'Desserts'],
+      avgRating: 4.1,
+      parentId: '2456',
+      avgRatingString: '4.1',
+      totalRatingsString: '5.1K+',
+      sla: {
+        deliveryTime: 25,
+        lastMileTravel: 0.1,
+        serviceability: 'SERVICEABLE',
+        slaString: '20-25 mins',
+        lastMileTravelString: '0.1 km',
+        iconType: 'ICON_TYPE_EMPTY',
+      },
+      availability: {
+        nextCloseTime: '2024-11-16 00:59:00',
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: 'F',
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: '₹175 OFF',
+        subHeader: 'ABOVE ₹999',
+        discountTag: 'FLAT DEAL',
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: 'ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT',
+        differentiatedUiMediaDetails: {
+          mediaType: 'ADS_MEDIA_ENUM_IMAGE',
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: 'RESTAURANT_DISPLAY_TYPE_DEFAULT',
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: '--',
+        },
+      },
+      ratingsDisplayPreference: 'RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY',
+    },
+  },
+  {
+    info: {
+      id: '63005',
+      name: 'LunchBox - Meals and Thalis',
+      cloudinaryImageId:
+        'RX_THUMBNAIL/IMAGES/VENDOR/2024/10/16/50341c69-4dce-48d5-a308-96e4c974d909_63005.jpg',
+      locality: 'Sudhakar Thorat Colony',
+      areaName: 'Karve Nagar',
+      costForTwo: '₹200 for two',
+      cuisines: [
+        'Biryani',
+        'North Indian',
+        'Punjabi',
+        'Healthy Food',
+        'Desserts',
+        'Beverages',
+      ],
+      avgRating: 4.4,
+      parentId: '4925',
+      avgRatingString: '4.4',
+      totalRatingsString: '4.8K+',
+      sla: {
+        deliveryTime: 29,
+        lastMileTravel: 3.8,
+        serviceability: 'SERVICEABLE',
+        slaString: '25-30 mins',
+        lastMileTravelString: '3.8 km',
+        iconType: 'ICON_TYPE_EMPTY',
+      },
+      availability: {
+        nextCloseTime: '2024-11-15 23:59:00',
+        opened: true,
+      },
+      badges: {
+        textExtendedBadges: [
+          {
+            iconId: 'guiltfree/GF_Logo_android_3x',
+            shortDescription: 'options available',
+            fontColor: '#7E808C',
+          },
+        ],
+      },
+      isOpen: true,
+      type: 'F',
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: '',
+                  fontColor: '#7E808C',
+                  iconId: 'guiltfree/GF_Logo_android_3x',
+                  shortDescription: 'options available',
+                },
+              },
+            ],
+          },
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: '50% OFF',
+        subHeader: 'UPTO ₹100',
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: 'ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT',
+        differentiatedUiMediaDetails: {
+          mediaType: 'ADS_MEDIA_ENUM_IMAGE',
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: 'RESTAURANT_DISPLAY_TYPE_DEFAULT',
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: '--',
+        },
+      },
+      ratingsDisplayPreference: 'RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY',
+    },
+  },
+  {
+    info: {
+      id: '23677',
+      name: "McDonald's",
+      cloudinaryImageId:
+        'RX_THUMBNAIL/IMAGES/VENDOR/2024/9/18/0a788dae-72e2-4717-83a3-9ab29bfab889_23677.jpg',
+      locality: 'Erandwane',
+      areaName: 'Kothrud',
+      costForTwo: '₹400 for two',
+      cuisines: ['Burgers', 'Beverages', 'Cafe', 'Desserts'],
+      avgRating: 4.5,
+      parentId: '630',
+      avgRatingString: '4.5',
+      totalRatingsString: '32K+',
+      sla: {
+        deliveryTime: 25,
+        lastMileTravel: 3.8,
+        serviceability: 'SERVICEABLE',
+        slaString: '25-30 mins',
+        lastMileTravelString: '3.8 km',
+        iconType: 'ICON_TYPE_EMPTY',
+      },
+      availability: {
+        nextCloseTime: '2024-11-16 02:45:00',
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      aggregatedDiscountInfoV2: {},
+      type: 'F',
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: 'ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT',
+        differentiatedUiMediaDetails: {
+          mediaType: 'ADS_MEDIA_ENUM_IMAGE',
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: 'RESTAURANT_DISPLAY_TYPE_DEFAULT',
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: '4.2',
+          ratingCount: '4.7K+',
+        },
+        source: 'GOOGLE',
+        sourceIconImageId: 'v1704440323/google_ratings/rating_google_tag',
+      },
+      ratingsDisplayPreference: 'RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY',
+    },
+  },
+  {
+    info: {
+      id: '688733',
+      name: 'Burger King',
+      cloudinaryImageId:
+        'RX_THUMBNAIL/IMAGES/VENDOR/2024/6/11/60497022-434e-4d21-bc0e-813fea770ae5_688733.JPG',
+      locality: 'Mohite Paradise',
+      areaName: 'Sinhagad Road',
+      costForTwo: '₹350 for two',
+      cuisines: ['Burgers', 'American'],
+      avgRating: 4.3,
+      parentId: '166',
+      avgRatingString: '4.3',
+      totalRatingsString: '5.0K+',
+      sla: {
+        deliveryTime: 36,
+        lastMileTravel: 4.9,
+        serviceability: 'SERVICEABLE',
+        slaString: '35-40 mins',
+        lastMileTravelString: '4.9 km',
+        iconType: 'ICON_TYPE_EMPTY',
+      },
+      availability: {
+        nextCloseTime: '2024-11-16 03:00:00',
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      aggregatedDiscountInfoV2: {},
+      type: 'F',
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: 'ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT',
+        differentiatedUiMediaDetails: {
+          mediaType: 'ADS_MEDIA_ENUM_IMAGE',
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: 'RESTAURANT_DISPLAY_TYPE_DEFAULT',
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: '--',
+        },
+      },
+      ratingsDisplayPreference: 'RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY',
+    },
+  },
+  {
+    info: {
+      id: '34724',
+      name: 'Subway',
+      cloudinaryImageId:
+        'RX_THUMBNAIL/IMAGES/VENDOR/2024/10/25/5e165c84-8c7c-4a60-8bf9-85baf25ecd9d_34724.jpg',
+      locality: 'Warje',
+      areaName: 'Warje',
+      costForTwo: '₹350 for two',
+      cuisines: ['Healthy Food', 'Salads', 'Snacks', 'Desserts', 'Beverages'],
+      avgRating: 4.2,
+      parentId: '2',
+      avgRatingString: '4.2',
+      totalRatingsString: '6.0K+',
+      sla: {
+        deliveryTime: 28,
+        lastMileTravel: 0.8,
+        serviceability: 'SERVICEABLE',
+        slaString: '25-30 mins',
+        lastMileTravelString: '0.8 km',
+        iconType: 'ICON_TYPE_EMPTY',
+      },
+      availability: {
+        nextCloseTime: '2024-11-15 23:59:00',
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: 'F',
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: '₹150 OFF',
+        subHeader: 'ABOVE ₹299',
+        discountTag: 'FLAT DEAL',
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: 'ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT',
+        differentiatedUiMediaDetails: {
+          mediaType: 'ADS_MEDIA_ENUM_IMAGE',
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: 'RESTAURANT_DISPLAY_TYPE_DEFAULT',
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: '--',
+        },
+      },
+      ratingsDisplayPreference: 'RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY',
+    },
+  },
+];
